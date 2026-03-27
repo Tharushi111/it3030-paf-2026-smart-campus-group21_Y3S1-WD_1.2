65508dba3272cd6a3169b332c891147d25f065ce
Tharushi111 <gaminiparanagama2@gmail.com>
2026-03-27 12:49:54 +0530

    create home page ui

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+    return (
+      <div className="p-10">
+        <h1 className="text-3xl font-bold text-orange-600">
+          Welcome to CampusNexus
+        </h1>
+  
+        <p className="mt-4 text-gray-600">
+          Smart campus management system.
+        </p>
+      </div>
+    );
+  }
